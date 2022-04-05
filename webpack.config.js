fe814e3dae0b548b703d139b6b54ccb0d83a5adf
kyrysh <victoria.tyagleeva@gmail.com>
2022-04-05 10:30:05 +0300

    Step 8: tests

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    //mode: 'development',
+    mode: process.env.NODE_ENV || 'development',
     entry: './src/index.js',
     devtool: 'inline-source-map',
     devServer: {
@@ -24,10 +25,10 @@ module.exports = {
     },
     module: {
         rules: [
-            {
+            /*{
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
-            },
+            },*/
             {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader'],
@@ -39,58 +40,3 @@ module.exports = {
         ],
     },
 };
-
-/*const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const isProduction = process.env.NODE_ENV == 'production';
-
-
-const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
-};
-
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-    } else {
-        config.mode = 'development';
-        config.devtool = 'inline-source-map';
-    }
-    return config;
-};
-*/
