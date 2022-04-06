257028cad6bf47f10b53258fa75c18da083fe6f4
kyrysh <victoria.tyagleeva@gmail.com>
2022-04-06 10:38:15 +0300

    Step 8: tests

--- src/i18n.js
+++ src/i18n.js
@@ -3,14 +3,14 @@ import ru from './locales/ru.js';
 import app from './app.js';
 
 export default () => {
-  const i18n = i18n.createInstance();
-  i18n.init({
+  const i18nInstance = i18n.createInstance();
+  i18nInstance.init({
     lng: 'ru',
     debug: true,
     resources: {
       ru,
     }
-  }).then (app(i18n));
+  }).then (app(i18nInstance));
 };
 
 
